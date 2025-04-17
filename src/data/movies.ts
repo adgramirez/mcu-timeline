1ce140ba7da37a75f5249114f37b9f873edd43b0
Aaron Daniel Ramirez <adgramirez@addu.edu.ph>
2025-04-17 22:10:17 +0800

    First commit with sacred timeline map

---
+++ src/data/movies.ts
@@ -0,0 +1,13 @@
+export const movies = [
+  {
+    title: "Iron Man",
+    poster: "/images/ironman.jpg",
+    position: { x: "15%", y: "40%" },
+  },
+  {
+    title: "Endgame",
+    poster: "/images/avengers.jpg",
+    position: { x: "65%", y: "50%" },
+  },
+  // Add more here
+];
